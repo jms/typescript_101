4251d6a5f7775796febe4ba8720852dea7699f1f
Jeronimo F. Martinez Sanchez <jms@rz0r.net>
2016-05-19 23:14:17 -0600

    adding gulp file reference typescript Docs

---
+++ dist/test.js
@@ -0,0 +1,26 @@
+var test = 10;
+var cars_brands = ['Ferrari', 'Aston Martin', 'Acura'];
+var yes = true;
+function total(x, y) {
+    return x + y;
+}
+var firstname;
+var lastname;
+var fullname;
+firstname = 'joe';
+lastname = 'smith';
+fullname = firstname + " " + lastname;
+var x;
+var y;
+var a;
+x = '1';
+y = 2;
+a = x + y;
+document.body.innerHTML += " " + a;
+var pets = ["fido", "spot", "seeker"];
+var myPets;
+pets.forEach(function (concat) {
+    myPets = myPets + concat + ", ";
+});
+document.body.innerHTML = "My pets are " + " " + pets;
+var e;
